22f16ba5553b5e19c64ddef78a4d39fea5d336d4
Dustin Wang <dustinrwang@gmail.com>
2017-11-10 01:36:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,19 +1,9 @@
 // Write your code in this file!
 function scuberGreetingForFeet(distance) {
-  let distance;
   if (distance < 400) {
     return "This one is on me!";
-  } else if (distance > 2000 && distance < 2500) {
-    return 'I will gladly take your thirty bucks.';
-  } else if (distance > 2500) {
-    return 'No can do.';
-  }
-
-  function ternaryCheckCity(city) {
-    return (city='NYC') ? 'Ok, sounds good.'
-    : (city!='NYC') ? 'No go.'
-  }
-
+  } 
+}
   /* Goal - Does the city = NYC? Yes = return ok, No = return no go
    1. A => return '
    2. B => 'No go'
