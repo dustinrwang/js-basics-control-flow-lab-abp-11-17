0bc0b70ab79de524428d54a0a74f7714e24806b0
Dustin Wang <dustinrwang@gmail.com>
2017-11-10 01:33:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,12 +8,3 @@ function scuberGreetingForFeet(distance) {
     return 'No can do.';
   }
 
-function ternaryCheckCity(city) {
-  return (city='NYC') ? 'Ok, sounds good.'
-  : (city!='NYC') ? 'No go.'
-}
-
-/* Goal - Does the city = NYC? Yes = return ok, No = return no go
- 1. A => return '
- 2. B => 'No go'
-*/
